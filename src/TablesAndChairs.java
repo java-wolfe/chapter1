8672c820b4a6ecc84b58345f9532cf6519c55292
DakotaWolfe <54542632+DakotaWolfe@users.noreply.github.com>
2019-08-28 09:37:48 -0500

    update newest project

---
+++ src/TablesAndChairs.java
@@ -0,0 +1,24 @@
+public class TablesAndChairs {
+    public static void main(String[] args){
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("C                                                               C");
+        System.out. println("SEATSEATSEATSEAT        TableTableTable          SEATSEATSEATSEAT");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+        System.out. println("L              L        L             L          L              L");
+
+    }
+}
---
+++ src/TacoTruck.java
@@ -0,0 +1,18 @@
+public class TacoTruck {
+    public static void main(String[] args){
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruck                                    TacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruckTacoTruck");
+        System.out.println("TacoTruckWWWWWWWWWTacoTruckTacoTruckTacoTruckTacoTruckWWWWWWWWWTacoTruckTacoTruck");
+        
+    }
+}
