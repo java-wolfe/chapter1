5f74fef2bac333c24b364d5b4633c3d0217d386c
DakotaWolfe <54542632+DakotaWolfe@users.noreply.github.com>
2019-08-29 09:28:49 -0500

    update random

---
+++ src/MovieQuote.java
@@ -0,0 +1,7 @@
+public class MovieQuote {
+    public static void main(String[] args){
+        System.out. println("Ounce you've accepted");
+        System.out. println("your flaws, no one can");
+        System.out. println("use them against you");
+    }
+}
